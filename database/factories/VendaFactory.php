6d6f62bb79a499e3fdc4d223893a816c9df816ab
PamelaStieven <stieven.pamela13@gmail.com>
2026-09-11 14:21:24 -0300

    .

--- database/factories/VendaFactory.php
+++ database/factories/VendaFactory.php
@@ -3,14 +3,26 @@
 namespace Database\Factories;
 
 use App\Models\Venda;
+use App\Models\Usuario;
+use App\Models\Funcionario;
+use App\Models\Bicicleta;
+use App\Models\Acessorio;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class VendaFactory extends Factory
 {
+    protected $model = Venda::class;
+
     public function definition(): array
     {
         return [
-            //
+            'usuario_id' => Usuario::factory(),
+            'funcionario_id' => Funcionario::factory(),
+            'bicicleta_id' => Bicicleta::factory(),
+            'acessorio_id' => Acessorio::factory(),
+            'quantidade' => $this->faker->numberBetween(1, 5),
+            'data_venda' => $this->faker->dateTimeBetween('-1 year', 'now'),
         ];
     }
 }
+
