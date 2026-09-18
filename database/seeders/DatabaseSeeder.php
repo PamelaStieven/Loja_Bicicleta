7787d2d4b3d848bff01f63fc896eb2f6f4706598
PamelaStieven <stieven.pamela13@gmail.com>
2026-09-18 14:25:48 -0300

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,18 +3,16 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Usuario;
-use App\Models\Bicicleta;
-use App\Models\Acessorios;
-use App\Models\Venda; 
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Usuario::factory(10)->create();
-        Bicicleta::factory(10)->create();
-        Acessorios::factory(10)->create();
-        Venda::factory(10)->create(); 
+        $this->call([
+            UsuarioSeeder::class,
+            BicicletaSeeder::class,
+            AcessoriosSeeder::class,
+            VendaSeeder::class,
+        ]);
     }
 }
