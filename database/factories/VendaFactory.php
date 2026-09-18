ee2f41f3e0ce65f3146506d0c9343ef55676ef1c
PamelaStieven <stieven.pamela13@gmail.com>
2026-09-18 14:22:03 -0300

    .

--- database/factories/VendaFactory.php
+++ database/factories/VendaFactory.php
@@ -2,27 +2,26 @@
 
 namespace Database\Factories;
 
-use App\Models\Venda;
-use App\Models\Usuario;
-use App\Models\Funcionario;
-use App\Models\Bicicleta;
-use App\Models\Acessorio;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Usuario;
+use App\Models\Bicicleta;
+use App\Models\Acessorios; // Ou Acessorio, conforme o nome no seu projeto
 
 class VendaFactory extends Factory
 {
-    protected $model = Venda::class;
-
     public function definition(): array
     {
+        $quantidade = $this->faker->numberBetween(1, 3);
+        $valorUnitario = $this->faker->randomFloat(2, 50, 2000);
+
         return [
-            'usuario_id' => Usuario::factory(),
-            'funcionario_id' => Funcionario::factory(),
+            'usuario_id' => Usuario::factory()->state(['tipo' => 'usuario']),
+            'funcionario_id' => Usuario::factory()->state(['tipo' => 'funcionario']),
             'bicicleta_id' => Bicicleta::factory(),
-            'acessorio_id' => Acessorio::factory(),
-            'quantidade' => $this->faker->numberBetween(1, 5),
-            'data_venda' => $this->faker->dateTimeBetween('-1 year', 'now'),
+            'acessorio_id' => null, 
+            'quantidade' => $quantidade,
+            'valor_total' => $quantidade * $valorUnitario,
+            'data_venda' => $this->faker->date(),
         ];
     }
 }
-
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Seeder;
 use App\Models\Usuario;
 use App\Models\Bicicleta;
 use App\Models\Acessorios;
+use App\Models\Venda; 
 
-class DatabaseSeeder extends Seeder {
-    public function run(): void {
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
         Usuario::factory(10)->create();
         Bicicleta::factory(10)->create();
         Acessorios::factory(10)->create();
-        Vendas::factory(10)->create();
+        Venda::factory(10)->create(); 
     }
 }
